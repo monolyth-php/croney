f2b476ba072ba5ada285be3ab667ff0e4e6a842a
Marijn Ophorst <marijn@monomelodies.nl>
2022-09-30 15:24:03 +0200

    add an actual psr/log implementation for this

--- src/ErrorLogger.php
+++ src/ErrorLogger.php
@@ -2,20 +2,54 @@
 
 namespace Monolyth\Croney;
 
-class ErrorLogger
+use Psr\Log\LoggerInterface;
+use Stringable;
+
+class ErrorLogger implements LoggerInterface
 {
-    /**
-     * @param string $name
-     * @param array $args
-     */
-    public function __call(string $name, array $args)
+    public function emergency(string|Stringable $message, array $context = []): void
     {
-        if (preg_match('@^add[A-Z]@', $name)
-            && isset($args[0])
-            && is_string($args[0])
-        ) {
-            fwrite(STDERR, $args[0]."\n");
-        }
+        $this->log(STDERR, $message, $context);
+    }
+
+    public function alert(string|Stringable $message, array $context = []): void
+    {
+        $this->log(STDERR, $message, $context);
+    }
+
+    public function critical(string|Stringable $message, array $context = []): void
+    {
+        $this->log(STDERR, $message, $context);
+    }
+
+    public function error(string|Stringable $message, array $context = []): void
+    {
+        $this->log(STDERR, $message, $context);
+    }
+
+    public function warning(string|Stringable $message, array $context = []): void
+    {
+        $this->log(STDERR, $message, $context);
+    }
+
+    public function notice(string|Stringable $message, array $context = []): void
+    {
+        $this->log(STDERR, $message, $context);
+    }
+
+    public function info(string|Stringable $message, array $context = []): void
+    {
+        $this->log(STDOUT, $message, $context);
+    }
+
+    public function debug(string|Stringable $message, array $context = []): void
+    {
+        $this->log(STDOUT, $message, $context);
+    }
+
+    public function log($level, string|Stringable $message, array $context = []): void
+    {
+        fwrite($level, "$message\n");
     }
 }
 
