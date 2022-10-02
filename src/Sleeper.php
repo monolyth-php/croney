0d0ecd66df106878caa213d2bcb07404158aa67d
Marijn Ophorst <marijn@monomelodies.nl>
2022-10-02 18:07:17 +0200

    add sleeper so we can test without having to wait minutes

---
+++ src/Sleeper.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Monolyth\Croney;
+
+/**
+ * Helper class to let the task manager sleep. Can be overrided for unit tests.
+ * See README.md for an example.
+ */
+class Sleeper
+{
+    private int $currentTime;
+
+    public function __construct()
+    {
+        $this->currentTime = strtotime(date('Y-m-d H:i:00'));
+    }
+
+    public function snooze(int $seconds) : void
+    {
+        sleep($seconds);
+    }
+
+    protected function advanceInternalClock() : void
+    {
+        $this->currentTime = strtotime('+1 minute', $this->currentTime);
+    }
+
+    public function getDate(string $format = 'Y-m-d H:i') : string
+    {
+        return date($format, $this->currentTime);
+    }
+}
+
