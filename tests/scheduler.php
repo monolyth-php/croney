5f1a3f02b0a6c865741be524042bb055be22f031
Marijn Ophorst <marijn@monomelodies.nl>
2022-10-02 18:47:55 +0200

    update tests

--- tests/scheduler.php
+++ tests/scheduler.php
@@ -5,32 +5,33 @@ use Gentry\Gentry\Wrapper;
 
 /** Tests for the Croney scheduler */
 return function () : Generator {
-    $scheduler = new Scheduler;
+    $scheduler = new Wrapper(new Scheduler);
 
     $this->beforeEach(function () use ($scheduler) {
-        (new Wrapper($scheduler))->overrideOptions([]);
+        $scheduler->overrideOptions([]);
     });
 
     /** We can set a job which will be processed */
     yield function () use ($scheduler) {
         $run = false;
-        $scheduler['test'] = function () use (&$run) {
+        $scheduler->offsetSet('test', function () use (&$run) {
             $run = true;
-        };
-        (new Wrapper($scheduler))->process();
+        });
+        $scheduler->process();
         assert($run === true);
     };
 
     /** A job scheduled for the future won't be run */
     yield function () use ($scheduler) {
         $run = false;
-        $scheduler['test'] =
+        $scheduler->offsetSet('test',
             #[RunAt("2300-01-01")]
             function () use (&$run) {
                 $run = true;
-            };
+            }
+        );
         ob_start();
-        (new Wrapper($scheduler))->process();
+        $scheduler->process();
         $output = trim(ob_get_clean());
         assert($run === false);
         assert($output === "Skipping test due to RunAt configuration.");
@@ -39,7 +40,7 @@ return function () : Generator {
     /** We can override duration - setting to 2 will cause process to run twice */
     yield function () use ($scheduler) {
         $run = 0;
-        $scheduler = new class (2) extends Scheduler {
+        $scheduler = new Wrapper(new class (2) extends Scheduler {
             public function __construct(int $duration)
             {
                 parent::__construct($duration);
@@ -50,18 +51,18 @@ return function () : Generator {
                     }
                 };
             }
-        };
-        $scheduler['test'] = function () use (&$run) {
+        });
+        $scheduler->offsetSet('test', function () use (&$run) {
             $run++;
-        };
-        (new Wrapper($scheduler))->process();
+        });
+        $scheduler->process();
         assert($run === 2);
     };
 
     /** A job configured to only run on even minutes will be run once, even with duration=2 */
     yield function () use ($scheduler) {
         $run = 0;
-        $scheduler = new class (2) extends Scheduler {
+        $scheduler = new Wrapper(new class (2) extends Scheduler {
             public function __construct(int $duration)
             {
                 parent::__construct($duration);
@@ -72,14 +73,15 @@ return function () : Generator {
                     }
                 };
             }
-        };
-        $scheduler['test'] =
+        });
+        $scheduler->offsetSet('test',
             #[RunAt("Y-m-d H:[0-5][02468]")]
             function () use (&$run) {
                 $run++;
-            };
+            }
+        );
         ob_start();
-        (new Wrapper($scheduler))->process();
+        $scheduler->process();
         $output = trim(ob_get_clean());
         assert($run === 1);
         assert($output === "Skipping test due to RunAt configuration.");
@@ -87,9 +89,8 @@ return function () : Generator {
 
     /** We can override options and get the currently set value */
     yield function () use ($scheduler) {
-        $wrapped = new Wrapper($scheduler);
-        $wrapped->overrideOptions(['--verbose']);
-        $options = $wrapped->getOptions();
+        $scheduler->overrideOptions(['--verbose']);
+        $options = $scheduler->getOptions();
         assert($options->getOption('v') === 1);
     };
 
@@ -102,7 +103,7 @@ return function () : Generator {
                 print "I ran!";
             }
         }
-        $scheduler['test'] = Foo::class;
+        $scheduler->offsetSet('test', Foo::class);
         ob_start();
         $scheduler->process();
         $output = trim(ob_get_clean());
@@ -118,7 +119,7 @@ return function () : Generator {
                 print "I ran!";
             }
         }
-        $scheduler['test'] = [new Bar, 'test'];
+        $scheduler->offsetSet('test', [new Bar, 'test']);
         ob_start();
         $scheduler->process();
         $output = trim(ob_get_clean());
@@ -131,11 +132,34 @@ return function () : Generator {
         {
             print "I ran!";
         }
-        $scheduler['test'] = 'test';
+        $scheduler->offsetSet('test', 'test');
         ob_start();
         $scheduler->process();
         $output = trim(ob_get_clean());
         assert($output === 'I ran!');
+    };
+
+    /** The Sleeper can snooze and return current time */
+    yield function () {
+        $sleeper = new Wrapper(new Sleeper);
+        $now = strtotime(date('Y-m-d H:i'));
+        $sleeper->snooze(1);
+        assert($sleeper->getDate() == date('Y-m-d H:i', strtotime('+1 minute', $now)));
+        assert($sleeper->getDate('Y-m-01') == date('Y-m-01'));
+    };
+
+    /** The RunAt attribute correctly stores and returns dates */
+    $object = new Wrapper(new Monolyth\Croney\RunAt('1999-12-31 23:59'));
+    /** getDatetimeString yields $result === 'blarps' */
+    yield function () use ($object) {
+        $result = $object->getDatetimeString();
+        assert($result === '1999-12-31 23:59');
+    };
+    
+    /** getDateTime yields an instanceof DateTime */
+    yield function () use ($object) {
+        $result = $object->getDateTime();
+        assert($result instanceof DateTime);
     };
 };
 
