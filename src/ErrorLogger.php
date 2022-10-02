c944a17b4d88f6d8c73405e083dd1615f917eab4
Marijn Ophorst <marijn@monomelodies.nl>
2022-10-02 18:47:27 +0200

    mark these as untestable - they could be tested, but no point

--- src/ErrorLogger.php
+++ src/ErrorLogger.php
@@ -4,49 +4,59 @@ namespace Monolyth\Croney;
 
 use Psr\Log\LoggerInterface;
 use Stringable;
+use Gentry\Gentry\Untestable;
 
 class ErrorLogger implements LoggerInterface
 {
+    #[Untestable]
     public function emergency(string|Stringable $message, array $context = []): void
     {
         $this->log(STDERR, $message, $context);
     }
 
+    #[Untestable]
     public function alert(string|Stringable $message, array $context = []): void
     {
         $this->log(STDERR, $message, $context);
     }
 
+    #[Untestable]
     public function critical(string|Stringable $message, array $context = []): void
     {
         $this->log(STDERR, $message, $context);
     }
 
+    #[Untestable]
     public function error(string|Stringable $message, array $context = []): void
     {
         $this->log(STDERR, $message, $context);
     }
 
+    #[Untestable]
     public function warning(string|Stringable $message, array $context = []): void
     {
         $this->log(STDERR, $message, $context);
     }
 
+    #[Untestable]
     public function notice(string|Stringable $message, array $context = []): void
     {
         $this->log(STDERR, $message, $context);
     }
 
+    #[Untestable]
     public function info(string|Stringable $message, array $context = []): void
     {
         $this->log(STDOUT, $message, $context);
     }
 
+    #[Untestable]
     public function debug(string|Stringable $message, array $context = []): void
     {
         $this->log(STDOUT, $message, $context);
     }
 
+    #[Untestable]
     public function log($level, string|Stringable $message, array $context = []): void
     {
         fwrite($level, "$message\n");
